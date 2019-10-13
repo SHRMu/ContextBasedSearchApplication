f08a257a04f56b6e2e7cbe0fb9d8f44893694f12
Shouran Mu <shouran.mu@gmail.com>
2019-10-13 11:54:53 +0200

    update

--- src/main/java/de/tu/darmstadt/controller/IndexController.java
+++ src/main/java/de/tu/darmstadt/controller/IndexController.java
@@ -1,12 +1,16 @@
 package de.tu.darmstadt.controller;
 
+import de.tu.darmstadt.model.Search;
 import de.tu.darmstadt.service.EsRestService;
+import de.tu.darmstadt.service.ModelPredService;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -16,6 +20,8 @@ public class IndexController {
 
     @Autowired
     EsRestService restService;
+    @Autowired
+    ModelPredService modelService;
 
     @RequestMapping("/")
     public String index() {
@@ -31,6 +37,24 @@ public class IndexController {
         model.addAttribute("flist", fileList);
         model.addAttribute("keyword", keyword.toLowerCase());
         return "result";
+    }
+
+    @RequestMapping(value = "/data", method = RequestMethod.POST)
+    @ResponseBody
+    public JSONArray searchData(@RequestBody Search search, HttpServletRequest request){
+        String keyword = search.getKeyword();
+        System.out.println("input keywords :" + keyword);
+        ArrayList<String> predict= modelService.invokeModel(keyword);
+        JSONArray result = new JSONArray();
+        JSONObject data ;
+        for (String word:
+                predict) {
+            data = new JSONObject();
+            data.put("word"," "+word);
+            result.add(data);
+        }
+        System.out.println(request.toString());
+        return result;
     }
 
 }
